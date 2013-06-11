2956334768b6ae9380e395a997dc0a47f547674e
Javier Olaechea <pirata@gmail.com>
2013-06-11 00:43:26 -0400

    Fixed Not Immutable variables in default arguments.
          See this[1] for more details
    
          [1]: https://gist.github.com/PuercoPop/5754504

--- request.py
+++ request.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 class Request(object):
-	def __init__(self, method = "GET", version = "HTTP/1.1", route = '/', headers = {}, get_vars = {}, body = None):
-		self.method = method
+        def __init__(self, method="GET", version="HTTP/1.1",
+                     route='/', headers=None, get_vars=None, body=None):
+
+                if headers is None:
+                        headers = {}
+                if get_vars is None:
+                        get_vars = {}
+
+                self.method = method
 		self.headers = headers
 		self.route = route
 		self.body = body
